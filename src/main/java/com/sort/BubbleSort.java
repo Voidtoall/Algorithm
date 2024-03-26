b83f73fd6a6c917d76c5478c023fb95c17f8504d
zhangyaohai@sina.com <zhangyaohai@sina.com>
2024-03-26 21:59:29 +0800

    bubblesort

---
+++ src/main/java/com/sort/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.sort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("原始数组:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("\n排序后的数组:");
+        printArray(arr);
+    }
+
+    // 冒泡排序函数
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // 交换相邻元素
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // 打印数组函数
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
